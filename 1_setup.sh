b4dc0a44b6ca7f85b88f8f81ca7bfa33836af7d5
thanasxda <15927885+thanasxda@users.noreply.github.com>
2020-05-15 22:46:29 +0300

    improve scripting

--- 1_setup.sh
+++ 1_setup.sh
@@ -35,7 +35,8 @@ echo -e "${restore}"                                                    ###
 # all underneath setup parts marked with many "!!!" need to be set according to your distro
 # for transposable compatibility in case it is not used for (K}ubuntu focal/groovy
 
-
+### first of all install aptitude to ease out package conflicts
+sudo apt -f install -y aptitude
 
 ####### GENERAL DIRECTORIES #########################################################################
 #####################################################################################################
@@ -50,8 +51,8 @@ mkdir -p $git && mkdir -p $tc
 #####################################################################################################
 ### optimizations press -y & enter
 printf 'y\n' | sudo dpkg-reconfigure dash
-sudo apt -f install -y ureadahead
-sudo apt -f install -y kexec-tools
+sudo aptitude -f install -y ureadahead
+sudo aptitude -f install -y kexec-tools
 sudo apt -f install -y && sudo apt --fix-missing install -y
 printf 'y\ny\n' | sudo dpkg-reconfigure kexec-tools
 
@@ -60,7 +61,7 @@ printf 'y\ny\n' | sudo dpkg-reconfigure kexec-tools
 ####### GIT CONFIGURATION ###########################################################################
 #####################################################################################################
 ### your git name & email - unhash and set up for personal usage
-sudo apt -f install -y git curl
+sudo aptitude -f install -y git curl
 #git config --global user.name thanasxda
 #git config --global user.email 15927885+thanasxda@users.noreply.github.com
 
@@ -75,7 +76,7 @@ echo LICENSES                    #
 echo -e "${restore}"             #
 ##################################
 echo ttf-mscorefonts-installer ttf-mscorefonts-installer/accepted-ttf-mscorefonts-installer-eula select true | sudo debconf-set-selections
-sudo apt -f install -y ttf-mscorefonts-installer
+sudo aptitude -f install -y ttf-mscorefonts-installer
 
 
 
@@ -101,11 +102,16 @@ echo -e "${restore}"                                                            
 #####################################################################################
 cd $basicsetup
 #
-sudo apt -f install -y rsync
+sudo aptitude -f install -y rsync
 
 chmod +x init.sh
 sudo \cp init.sh /init.sh
-sudo sed -i '1s#.*#@reboot root /init.sh#' /etc/crontab
+if grep -q "@reboot root /init.sh" /etc/crontab
+then
+echo "Flag exists"
+else
+sudo sed -i "\$a@reboot root /init.sh" /etc/crontab
+fi
 
 ### copy wallpaper & grub splash
 sudo rsync -v -K -a --force  MalakasUniverse /usr/share/wallpapers
@@ -156,6 +162,12 @@ echo -e "${restore}"             #
 swap=5000000
 swappiness=90
 ##################################
+if grep -q "/swapfile" /etc/fstab
+then
+echo "Flag exists"
+else
+sudo sed -i "\$a/swapfile    none    swap    sw    0    0" /etc/fstab
+fi
 sudo swapoff -a
 sudo dd if=/dev/zero of=/swapfile bs=$swap count=1024
 sudo chmod 600 /swapfile
@@ -177,13 +189,13 @@ echo GRUB CONFIG                    #
 echo -e "${restore}"                #
 #####################################
 ### switch off mitigations improving linux performance
-sudo sed -i '10s/.*/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash noibrs noibpb nopti nospectre_v2 nospectre_v1 l1tf=off nospec_store_bypass_disable no_stf_barrier mds=off spectre_v2_user=off spec_store_bypass_disable=off mitigations=off scsi_mod.use_blk_mq=1"/' /etc/default/grub
+sudo sed -i '/GRUB_CMDLINE_LINUX_DEFAULT/c\GRUB_CMDLINE_LINUX_DEFAULT="quiet splash noibrs noibpb nopti nospectre_v2 nospectre_v1 l1tf=off nospec_store_bypass_disable no_stf_barrier mds=off spectre_v2_user=off spec_store_bypass_disable=off mitigations=off scsi_mod.use_blk_mq=1"' /etc/default/grub
 ### set grub timeout
-sudo sed -i '8s/.*/GRUB_TIMEOUT=2/' /etc/default/grub
+sudo sed -i "/GRUB_TIMEOUT/c\GRUB_TIMEOUT=1" /etc/default/grub
 ### set grub min resolution
-sudo sed -i '24s/.*/GRUB_GFXMODE=1024x768/' /etc/default/grub
+sudo sed -i "/GRUB_GFXMODE/c\GRUB_GFXMODE=1024x768" /etc/default/grub
 ### set grub wallpaper
-sudo sed -i '12s#.*#GRUB_BACKGROUND="/boot/grub/splash.jpg"#' /etc/default/grub
+sudo sed -i '/GRUB_BACKGROUND/c\GRUB_BACKGROUND="/boot/grub/splash.jpg"' /etc/default/grub
 ### apply grub settings
 sudo update-grub2
 ### grub auto detection
@@ -199,7 +211,7 @@ sudo sed -i "\$aexport USE_CCACHE=1" ~/.bashrc
 sudo sed -i "\$aexport USE_PREBUILT_CACHE=1" ~/.bashrc
 sudo sed -i "\$aexport PREBUILT_CACHE_DIR=~/.ccache" ~/.bashrc
 sudo sed -i "\$aexport CCACHE_DIR=~/.ccache" ~/.bashrc
-sudo sed -i "\$acache -M 30G >/dev/null" ~/.bashrc
+sudo sed -i "\$accache -M 30G >/dev/null" ~/.bashrc
 fi
 
 #hash out tested unstable for now
@@ -225,7 +237,7 @@ cd $git
 #
 ### add i386 architecture needed for env
 sudo dpkg --add-architecture i386
-sudo apt update
+sudo aptitude update
 git clone https://github.com/akhilnarang/scripts.git
 cd scripts/setup
 Keys.ENTER | ./android_build_env.sh
@@ -271,6 +283,16 @@ done
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 1378B444
 ### fetch keys llvm git
 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
+### obaif
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 957d2708a03a4626
+### kodi
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6d975c4791e7ee5e
+### git
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys a1715d88e1df1f24
+### usb stuff
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3729827454b8c8ac
+### appimage
+#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys4af9b16f75ef2fca
 
 ##################################
 ### fix groovy distro syncing for now
@@ -315,15 +337,33 @@ echo THANAS PACKAGES             #
 echo -e "${restore}"             #
 ##################################
 sudo apt update
-sudo apt -f install -y aptitude
 
-sudo aptitude -f install -y amd64-microcode android-sdk android-tools-adb android-tools-adbd android-tools-fastboot autoconf autoconf-archive autogen automake autopoint autotools-dev bash bc binfmt-support binutils-dev bison build-essential bzip2 ca-certificates ccache clang clang-11 clang-11-doc clang-format clang-format-11 clang-tidy clang-tools-11 clangd clangd-11 cmake curl dash desktop-base dkms dpkg-dev ecj expat fastjar file flatpak flex g++ gawk gcc gcc-10 gdebi gedit gettext git git-svn gnupg gparted gperf gstreamer1.0-qt5 help2man imagemagick intel-microcode java-propose-classpath kubuntu-restricted-extras kwrite lib32ncurses-dev lib32readline-dev lib32z1 lib32z1-dev libbz2-dev libc++-11-dev libc++abi-11-dev libc6-dev libc6-dev-i386 libcap-dev libclang-11-dev libclang-dev libclang1 libclang1-11 libelf-dev libexpat1-dev libffi-dev libfuzzer-11-dev libghc-bzlib-dev libgl1-mesa-dev libgmp-dev libjpeg8-dev libllvm-11-ocaml-dev libllvm-ocaml-dev libllvm11 liblz4-1 liblz4-1:i386 liblz4-dev liblz4-java liblz4-jni liblz4-tool liblzma-dev liblzma-doc liblzma5 libmpc-dev libmpfr-dev libncurses-dev libncurses5 libncurses5-dev libomp-11-dev libsdl1.2-dev libssl-dev libtool libtool-bin libvdpau-va-gl1 libvulkan1 libx11-dev libxml2 libxml2-dev libxml2-utils linux-libc-dev linux-tools-common lld lld-11 lldb llvm llvm-11 llvm-11-dev llvm-11-doc llvm-11-examples llvm-11-runtime llvm-dev llvm-runtime lzma lzma-alone lzma-dev lzop m4 make maven mesa-opencl-icd mesa-va-drivers mesa-vulkan-drivers nautilus ninja-build ocl-icd-libopencl1 openjdk-8-jdk openssh-client openssh-server optipng patch pigz pkg-config pngcrush python-all-dev python-clang python3.8 python3-distutils qt5-default rsync schedtool shtool snapd squashfs-tools subversion tasksel texinfo txt2man ubuntu-restricted-extras unzip vdpau-driver-all vlc vulkan-utils wget x11proto-core-dev xsltproc yasm zip zlib1g-dev
+sudo aptitude -f install -y amd64-microcode android-sdk android-tools-adb android-tools-adbd android-tools-fastboot autoconf autoconf-archive autogen automake autopoint autotools-dev bash bc binfmt-support binutils-dev bison build-essential bzip2 ca-certificates ccache clang clang-11 clang-11-doc clang-format clang-format-11 clang-tidy clang-tools-11 clangd clangd-11 cmake curl dash desktop-base dkms dpkg-dev ecj expat fastjar file flatpak flex g++ gawk gcc gcc-10 gdebi gedit gettext git git-svn gnupg gparted gperf gstreamer1.0-qt5 help2man imagemagick intel-microcode java-propose-classpath kubuntu-restricted-extras kwrite lib32ncurses-dev lib32readline-dev lib32z1 lib32z1-dev libbz2-dev libc++-11-dev libc++abi-11-dev libc6-dev libc6-dev-i386 libcap-dev libclang-11-dev libclang-dev libclang1 libclang1-11 libelf-dev libexpat1-dev libffi-dev libfuzzer-11-dev libghc-bzlib-dev libgl1-mesa-dev libgmp-dev libjpeg8-dev libllvm-11-ocaml-dev libllvm-ocaml-dev libllvm11 liblz4-1 liblz4-1:i386 liblz4-dev liblz4-java liblz4-jni liblz4-tool liblzma-dev liblzma-doc liblzma5 libmpc-dev libmpfr-dev libncurses-dev libncurses5 libncurses5-dev libomp-11-dev libsdl1.2-dev libssl-dev libtool libtool-bin libvdpau-va-gl1 libvulkan1 libx11-dev libxml2 libxml2-dev libxml2-utils linux-libc-dev linux-tools-common lld lld-11 lldb llvm llvm-11 llvm-11-dev llvm-11-doc llvm-11-examples llvm-11-runtime llvm-dev llvm-runtime lzma lzma-alone lzma-dev lzop m4 make maven mesa-opencl-icd mesa-va-drivers mesa-vulkan-drivers nautilus ninja-build ocl-icd-libopencl1 openjdk-8-jdk openssh-client openssh-server optipng patch pigz pkg-config pngcrush python-all-dev python-clang python3.8 python3-distutils qt5-default rsync schedtool shtool snapd squashfs-tools subversion tasksel texinfo txt2man ubuntu-restricted-extras unzip vdpau-driver-all vlc vulkan-utils wget x11proto-core-dev xsltproc yasm zip zlib1g-dev mpc
+
+### fwupd
+sudo aptitude -f install -y fwupd plasma-discover-backend-fwupd
+
+### list mesa drivers seperately
+sudo aptitude -f install -y libd3dadapter9-mesa libd3dadapter9-mesa-dev libegl-mesa0 libegl1-mesa-dev libgl1-mesa-dev libgl1-mesa-dri libgl1-mesa-glx libglapi-mesa libgles2-mesa-dev libglu1-mesa libglu1-mesa-dev libglx-mesa0 libosmesa6 libosmesa6-dev mesa-common-dev mesa-vdpau-drivers mesa-vulkan-drivers mir-client-platform-mesa-dev
 
 ### gcc arm
-sudo apt install -y gcc-aarch64-linux-gnu gcc-arm-linux-gnueabi gcc-10-aarch64-linux-gnu gcc-10-arm-linux-gnueabi
+#sudo aptitude -f install -y gcc-aarch64-linux-gnu gcc-arm-linux-gnueabi gcc-10-aarch64-linux-gnu gcc-10-arm-linux-gnueabi
+sudo aptitude -f install -y gcc-multilib
+sudo aptitude -f install -y gcc-10-multilib
+
+### ensure full clang
+sudo aptitude -f install -y llvm-11
+sudo aptitude -f install -y llvm
+sudo aptitude -f install -y clang-11 lld-11
+sudo aptitude -f install -y clang-10 lld-10
+sudo aptitude -f install -y gcc-10
+sudo aptitude -f install -y gcc clang binutils make flex bison bc build-essential libncurses-dev libssl-dev libelf-dev qt5-default
 
 ### ram cache stuff
-sudo apt install zlib1g zlib1g-dev libcryptsetup12 libcryptsetup-dev libjansson4 libjansson-dev
+sudo aptitude -f install -y zlib1g zlib1g-dev libcryptsetup12 libcryptsetup-dev libjansson4 libjansson-dev
+
+### kde
+sudo aptitude install -y muon kde-baseapps kde-plasma-desktop plasma-browser-integration
 
 ### npm
 sudo apt -f install -y npm && sudo apt -f install -y && sudo npm cache clean -f && sudo npm cache clean -f && sudo npm install npm@latest -g
@@ -332,19 +372,24 @@ sudo apt -f install -y npm && sudo apt -f install -y && sudo npm cache clean -f 
 ### SELECT EXPLICITLY FOR KDE PLASMA DESKTOP ENVIRONMENT! needs manual enabling from within settings
 sudo apt install -y plasma-workspace-wayland kwayland-integration wayland-protocols
 ### allow root privilege under wayland and supress output
-sudo sed -i '4s#.*#xhost +si:localuser:root >/dev/null#' ~/.bashrc
+if grep -q "xhost +si:localuser:root >/dev/null" ~/.bashrc
+then
+echo "Flag exists"
+else
+sudo sed -i "\$axhost +si:localuser:root >/dev/null" ~/.bashrc
+fi
 ### !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 ### extra thanas packages
 ### some listed purposefully seperate to avoid future conflicts or to distinguish packages from unique repo's or ppa's
-sudo apt -f install -y audacity diffuse gimp kodi kodi-pvr-hts kodi-wayland f2fs-tools rt-tests uget net-tools aircrack-ng wine32 wine shellcheck gnome-disk-utility
+sudo aptitude -f install -y audacity diffuse gimp kodi kodi-pvr-hts kodi-x11 kodi-wayland f2fs-tools rt-tests uget net-tools aircrack-ng wine32 wine shellcheck gnome-disk-utility putty
 
 ### make sure all is set up right
 sudo dpkg --configure -a && sudo apt update && sudo apt -f upgrade -y && sudo apt -f --fix-broken install -y && sudo apt -f --fix-missing install -y && sudo apt autoremove -y
 
-sudo apt -f install -y appimagelauncher
+sudo aptitude -f install -y appimagelauncher
 
-sudo apt -f install -y x264 x265
+sudo aptitude -f install -y x264 x265
 
 ### make sure all is set up right
 sudo dpkg --configure -a && sudo apt update && sudo apt -f upgrade -y && sudo apt -f --fix-broken install -y && sudo apt -f --fix-missing install -y && sudo apt autoremove -y
@@ -353,11 +398,16 @@ sudo dpkg --configure -a && sudo apt update && sudo apt -f upgrade -y && sudo ap
 mkdir -p ~/wine && cd ~/wine
 wget https://winscp.net/download/files/202005080143368dd0551d11a66577d4727edb0182a2/WinSCP-5.17.5-Portable.zip
 unzip -o WinSCP*
-rm -rf license* readme* WinSCP*.zip
+rm -rf license* readme* WinSCP*.zip WinSCP*.com
 
 ### extra .deb packages
 cd $source
 #
+wget https://github.com/balena-io/etcher/releases/download/v1.5.89/balena-etcher-electron_1.5.89_amd64.deb
+sudo dpkg -i balena-etcher*
+sudo apt -f install -y && sudo apt --fix-broken install -y
+rm -rf balena-etcher*
+
 wget https://download.cdn.viber.com/cdn/desktop/Linux/viber.deb
 sudo dpkg -i viber*
 sudo apt -f install -y && sudo apt --fix-broken install -y
@@ -407,9 +457,9 @@ sudo ./anbox_modules_fix.sh
 sudo apt -f install -y
 
 ### usb stuff
-#sudo add-apt-repository -y ppa:mkusb/ppa
-#sudo apt update
-#sudo apt -f install --install-recommends -y mkusb mkusb-nox usb-pack-efi
+sudo add-apt-repository -y ppa:mkusb/ppa
+sudo apt update
+sudo apt -f install --install-recommends -y mkusb mkusb-nox usb-pack-efi
 
 
 
@@ -437,19 +487,19 @@ git clone https://github.com/thanasxda/AnyKernel3.git
 git clone https://github.com/LionSec/katoolin.git
 
 ### whereisbssid script
-git clone https://github.com/Trackbool/WhereIsBSSID.git
+git clone --depth=1 https://github.com/Trackbool/WhereIsBSSID.git
 
 ### prebuilt llvm tc with lto=full pgo polly support
 cd $tc
 #
-git clone https://github.com/TwistedPrime/twisted-clang.git
+git clone --depth=1 https://github.com/TwistedPrime/twisted-clang.git
 mv twisted-clang clang
 cd $source
 
 
 
 ### make sure all is set up right
-sudo dpkg --configure -a && sudo apt update && sudo apt -f upgrade -y && sudo apt -f --fix-broken install -y && sudo apt -f --fix-missing install -y && sudo apt autoremove -y
+sudo dpkg --configure -a && sudo apt update && sudo apt -f full-upgrade -y && sudo apt -f --fix-broken install -y && sudo apt -f --fix-missing install -y && sudo apt autoremove -y
 
 ####### SETUP FINISHED ##############################################################################
 #####################################################################################################
@@ -468,8 +518,8 @@ echo -e "${restore}"                                                            
 ### can be done isolated as well on any distro, use ./build.sh
 cd $git
 git clone --depth=1 https://github.com/thanasxda/thanas-x86-64-kernel.git
-cd thanas-x86-64-kernel
-./1_build.sh
+cd thanas-x86-64-kernel && sudo chmod 755 *.sh
+./1*
 
 
 
