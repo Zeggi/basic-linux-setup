5f67b55ed2b3b8a7764d687e8efa8da7bd79489f
thanasxda <15927885+thanasxda@users.noreply.github.com>
2020-05-24 22:24:53 +0300

    add some vulkan pkgs

--- 1_setup.sh
+++ 1_setup.sh
@@ -345,7 +345,7 @@ sudo aptitude -f install -y amd64-microcode android-sdk android-tools-adb androi
 sudo aptitude -f install -y fwupd plasma-discover-backend-fwupd cpufrequtils
 
 ### list mesa drivers seperately
-sudo aptitude -f install -y libd3dadapter9-mesa libd3dadapter9-mesa-dev libegl-mesa0 libegl1-mesa-dev libgl1-mesa-dev libgl1-mesa-dri libgl1-mesa-glx libglapi-mesa libgles2-mesa-dev libglu1-mesa libglu1-mesa-dev libglx-mesa0 libosmesa6 libosmesa6-dev mesa-common-dev mesa-vdpau-drivers mesa-vulkan-drivers mir-client-platform-mesa-dev
+sudo aptitude -f install -y libd3dadapter9-mesa libd3dadapter9-mesa-dev libegl-mesa0 libegl1-mesa-dev libgl1-mesa-dev libgl1-mesa-dri libgl1-mesa-glx libglapi-mesa libgles2-mesa-dev libglu1-mesa libglu1-mesa-dev libglx-mesa0 libosmesa6 libosmesa6-dev mesa-common-dev mesa-vdpau-drivers mesa-vulkan-drivers mir-client-platform-mesa-dev vulkan-utils
 
 ### gcc arm
 #sudo aptitude -f install -y gcc-aarch64-linux-gnu gcc-arm-linux-gnueabi gcc-10-aarch64-linux-gnu gcc-10-arm-linux-gnueabi
